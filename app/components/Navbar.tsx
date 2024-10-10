930b4603095a87b0a08215ea88a41adbbbdc8761
nityapriya-sahu <sahunityapriya@gmail.com>
2024-10-10 11:10:09 +0530

    google auth & github auth complete

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import Logo from "@/public/logo.png";
+import { AuthModal } from "./AuthModal";
 
 export function Navbar() {
   return (
     <div className="flex py-5 items-center justify-between ">
       <Link href="/" className="flex item-center gap-2">
-        {/* <Image src={} alt="Logo" className='size-10'/> */}
-        <h4>Image Logo</h4>
-        <h4 className="text-3xl font-semibold">Calendar</h4>
+        <Image src={Logo} alt="Logo" className="size-10" />
+        <h4 className="text-3xl font-semibold">
+          MY<span className="text-blue-500"> - Calendar</span>
+        </h4>
       </Link>
+      <AuthModal />
     </div>
   );
 }
